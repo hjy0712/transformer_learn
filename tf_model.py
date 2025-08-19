96c1aa4f963fecb38725ced8129f97f6398cec03
hjy0712 <22332146@zju.edu.cn>
2025-08-19 16:28:46 +0800

    decoder layer

--- tf_model.py
+++ tf_model.py
@@ -169,15 +169,68 @@ class EncoderLayer(nn.Module):
         return x
 
 # ------------------测试编码器层-------------------
+# d_model = 512
+# max_len = 100
+# num_heads = 8
+# d_ff = 2048
+
+# encoder_layer = EncoderLayer(d_model, num_heads, d_ff)
+
+# input_sequence = torch.randn(5, max_len, d_model)
+
+# encoder_output = encoder_layer(input_sequence, None)
+# print(encoder_output.shape) # torch.Size([5, 100, 512])
+# ------------------------------------------------
+
+## 5. 解码器层
+class DecoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, d_ff, dropout=0.1):
+        super(DecoderLayer, self).__init__()
+        self.masked_self_attention = MultiHeadAttention(d_model, num_heads)
+        self.enc_dec_attention = MultiHeadAttention(d_model, num_heads)
+        self.feed_forward = FeedForward(d_model, d_ff)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.norm3 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+
+        # 掩码的自注意力层
+        self_attention_output = self.masked_self_attention(x, x, x, tgt_mask)
+        self_attention_output = self.dropout(self_attention_output)
+        x = x + self_attention_output
+        x = self.norm1(x)
+
+        # 编码器-解码器注意力层
+        enc_dec_attention_output = self.enc_dec_attention(x, encoder_output, encoder_output, src_mask)
+        enc_dec_attention_output = self.dropout(enc_dec_attention_output)
+        x = x + enc_dec_attention_output
+        x = self.norm2(x)
+
+        # 前馈网络层
+        feed_forward_output = self.feed_forward(x)
+        feed_forward_output = self.dropout(feed_forward_output)
+        x = x + feed_forward_output
+        x = self.norm3(x)
+
+        return x
+
+# ------------------测试解码器层-------------------
 d_model = 512
 max_len = 100
 num_heads = 8
 d_ff = 2048
+dropout = 0.1
 
-encoder_layer = EncoderLayer(d_model, num_heads, d_ff)
+decoder_layer = DecoderLayer(d_model, num_heads, d_ff, dropout)
 
-input_sequence = torch.randn(5, max_len, d_model)
+src_mask = torch.randn(1, max_len, max_len) > 0.5
+tgt_mask = torch.tril(torch.ones(max_len, max_len)).unsqueeze(0) == 0 # 因果掩码，下三角矩阵之后在第一维添加维度
 
-encoder_output = encoder_layer(input_sequence, None)
-print(encoder_output.shape) # torch.Size([5, 100, 512])
+input_sequence = torch.randn(1, max_len, d_model)
+encoder_output = torch.randn(1, max_len, d_model)
+
+decoder_output = decoder_layer(input_sequence, encoder_output, src_mask, tgt_mask)
+print(decoder_output.shape) # torch.Size([5, 100, 512])
 # ------------------------------------------------
